4cb7c3b7f96540da785fbda5bba7f6962d4213bc
Guoxiang Zhou <gzhou@anybotics.com>
2020-10-29 12:15:51 +0000

    Merge branch 'feature/minor_improvment' into 'master'
    [wide_angle_camera] Fix stack launching issues with VIO and WAC driver
    
    GitOrigin-RevId: 0758dc528f5458111abb5f11b843c84fa293cc07

--- src/nodelet.cpp
+++ src/nodelet.cpp
@@ -205,6 +205,7 @@ private:
   */
   void connectCb()
   {
+    std::lock_guard<std::mutex> scopedLock(connect_mutex_); // Grab the mutex.  Wait until we're done initializing
     if (!pubThread_)  // We need to connect
     {
       // Start the thread to loop through and publish messages
@@ -213,7 +214,6 @@ private:
     }
 
     NODELET_DEBUG_STREAM("Connect connectCb callback! The number of image_raw subscribers: " << it_pub_.getNumSubscribers() << ". The number of image subscribers: "<< pub_->getPublisher().getNumSubscribers());
-    std::lock_guard<std::mutex> scopedLock(connect_mutex_); // Grab the mutex.  Wait until we're done initializing
     // Check if we should disconnect (there are 0 subscribers to our data)
     if(it_pub_.getNumSubscribers() == 0 && pub_->getPublisher().getNumSubscribers() == 0)
     {
@@ -345,6 +345,18 @@ private:
     // SubscriberStatusCallback: http://docs.ros.org/melodic/api/roscpp/html/classros_1_1NodeHandle.html#ae4711ef282892176ba145d02f8f45f8d
     // cb will be called every time a new subscriber is connected to.
     image_transport::SubscriberStatusCallback cb = boost::bind(&SpinnakerCameraNodelet::connectCb, this);
+    // Start devicePoll first to trigger image streaming. This is needed because:
+    // When we launch this camera driver together with other nodes which subscribe to image_color or image_color_rect topic, if the other nodes
+    // are loaded first, subscribing to the image_color or image_color_rect topic, cb will not be triggered when the camera driver is loaded.
+    // As a result, we will not get image_color or image_color_rect streaming even when explicitly subscribing to these topics additionally (fishy).
+    // One solution is to unsubscribe to these topics from these nodes and cb will be triggered so that the camera will start image streaming.
+    // Another solution will be to explicit start the devicePoll thread to make the camera stream when launching the driver, which is what we do below.
+    if (!pubThread_)
+    {
+      // Start the thread to loop through and publish messages
+      pubThread_.reset(
+          new boost::thread(boost::bind(&any_spinnaker_camera_driver::SpinnakerCameraNodelet::devicePoll, this)));
+    }
     it_pub_ = it_->advertiseCamera("image_raw", 5, cb, cb);
 
     // Set up diagnostics
